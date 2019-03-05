dfd08e0498ab3ef357d602f702fee0c3640bf5b7
qipanda <qipan321@gmail.com>
2019-03-05 18:01:27 -0500

    adding cuda (NOT TESTED YET)

--- Models/SGNS.py
+++ Models/SGNS.py
@@ -4,6 +4,9 @@ import numpy as np
 import torch
 from sklearn.base import BaseEstimator, ClassifierMixin
 
+# CUDA variable for GPU usage if it exists
+USE_CUDA = torch.cuda.is_available()
+
 class SGNSModel(torch.nn.Module):
     def __init__(self, embedding_dim: int, c_vocab_len: int, w_vocab_len: int) -> None:
         """
@@ -58,6 +61,10 @@ class SGNSClassifier(BaseEstimator, ClassifierMixin):
         # Train a new model
         self.model_ = SGNSModel(self.embedding_dim, self.c_vocab_len, self.w_vocab_len)
 
+        # If GPU available, use them
+        if USE_CUDA:
+            self.model_ = self.model_.cuda()
+
         # set max threads and initialize the SGD optimizer
         torch.set_num_threads(self.torch_threads)
         optimizer = torch.optim.SGD(self.model_.parameters(), lr=self.lr)
@@ -81,13 +88,21 @@ class SGNSClassifier(BaseEstimator, ClassifierMixin):
                 # 3.) Before new batch, zero old gradient instance built up in model
                 self.model_.zero_grad()
 
-                # 4.) Forward pass to get prob of pos
-                pos_prob = self.model_(torch.tensor([x[:, 0]]), torch.tensor([x[:, 1]]))
+                # 4.) get input and target as torch tensors, use GPU is available
+                c, w = torch.tensor([x[:, 0]]), torch.tensor([x[:, 1]])
+                y_target_tensor = torch.tensor([y_target], dtype=torch.float).view(-1)
 
-                # 5.) Compute loss function
-                loss = self.loss_fn(pos_prob, torch.tensor([y_target], dtype=torch.float).view(-1))
+                if USE_CUDA:
+                    c, w = c.cuda(), w.cuda()
+                    y_target_tensor = y_target_tensor.cuda()
 
-                # 6.) Back pass then update based on gradient from back pass
+                # 5.) Forward pass to get prob of pos
+                pos_prob = self.model_(c, w)
+
+                # 6.) Compute loss function
+                loss = self.loss_fn(pos_prob, y_target_tensor)
+
+                # 7.) Back pass then update based on gradient from back pass
                 loss.backward()
                 optimizer.step()
             
@@ -97,20 +112,31 @@ class SGNSClassifier(BaseEstimator, ClassifierMixin):
         """
         Return list of predictions based on [self.pred_thresh]
         """
-        return self.model_(torch.tensor(X[:, 0]), torch.tensor(X[:, 1]))\
-            .detach().numpy() > self.pred_thresh
+        c, w = torch.tensor([x[:, 0]]), torch.tensor([x[:, 1]])
+        if USE_CUDA:
+            c, w = c.cuda(), w.cuda()
+            y_pred = self.model_(c, w).cpu().detach().numpy() > self.pred_thresh
+        else:
+            y_pred = self.model_(c, w).detach().numpy() > self.pred_thresh
+
+        return y_pred
 
     def predict_proba(self, X: np.ndarray) -> np.ndarray:
         """
         Return list of probabilitic predictions
         """
-        return self.model_(torch.tensor(X[:, 0]), torch.tensor(X[:, 1]))\
-            .detach().numpy()
+        c, w = torch.tensor([x[:, 0]]), torch.tensor([x[:, 1]])
+        if USE_CUDA:
+            c, w = c.cuda(), w.cuda()
+            y_pred = self.model_(c, w).cpu().detach().numpy()
+        else:
+            y_pred = self.model_(c, w).detach().numpy()
+
+        return y_pred
 
     def score(self, X: np.ndarray, y: np.ndarray) -> float:
         """
         Return accuracy average
         """
-        predictions = self.model_(torch.tensor(X[:, 0]), torch.tensor(X[:, 1]))
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
