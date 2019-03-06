32d239d5eeec19dd000da96d811ba5d483af34a4
qipanda <qipan321@gmail.com>
2019-03-06 15:58:51 -0500

    training 20 dim with 1.0 lr

--- Experiments/sgns-train.py
+++ Experiments/sgns-train.py
@@ -9,8 +9,20 @@ from Preprocessing.FullContextProcessor import FullContextProcessor
 # Import installed modules
 import torch
 
-# Load data from indexed version
-fcp = FullContextProcessor("../Data/ABC-News/abcnews-sgns-processed-idx.txt", sep="\t")
+# Load data and combine
+fcp = FullContextProcessor(data_fpath="../Data/ABC-News/abcnews-sgns-processed.txt", sep="\t")
+fcp.appendDf(data_fpath="../Data/Times-of-India/india-sgns-processed.txt", sep="\t")
+
+# Filter to c1-c2 pairs that have occured at least [occurance_thresh] times
+cpair_counts = fcp.df.loc[fcp.df["pos"]==1, :].groupby("c1-c2")["pos"].count()
+valid_cpairs = cpair_counts[cpair_counts >= 200]
+fcp.df = fcp.df.loc[fcp.df["c1-c2"].isin(valid_cpairs.keys()), :]
+
+# Create mappings and save indexed version
+fcp.createTwoWayMap(colname="c1-c2")
+fcp.createTwoWayMap(colname="word")
+fcp.convertColToIdx(colname="c1-c2")
+fcp.convertColToIdx(colname="word")
 
 # Turn data in X and y numpy arrays
 X = fcp.df.loc[:, ["c1-c2", "word"]].values
@@ -20,12 +32,14 @@ y = fcp.df.loc[:, "pos"].values
 sgns = SGNSClassifier(embedding_dim = 20,
                       c_vocab_len = len(fcp.df["c1-c2"].unique()), 
                       w_vocab_len = len(fcp.df["word"].unique()),
-                      lr = 1e-3,
-                      train_epocs = 20,
-                      torch_threads = 5,
+                      lr = 1.0,
+                      batch_size = 32,
+                      train_epocs = 10,
+                      shuffle = True,
+                      torch_threads = 7,
                       BCE_reduction = "mean",
-                      pred_thresh = 0.5,)
-print("training 20 dim...")
+                      pred_thresh = 0.5,
+                      log_fpath=".logs/sgns-20-train.log")
 sgns.fit(X, y)
 
 # Save model for later
@@ -36,25 +50,3 @@ torch.save(sgns.model_.state_dict(), "sgns-20.pt")
 #                    c_vocab_len = len(fcp.df["c1-c2"].unique()), 
 #                    w_vocab_len = len(fcp.df["word"].unique()),)
 # model.load_state_dict(torch.load("sgns-20.pt"))
-
-
-# Set up training for 300 hidden dim model
-sgns = SGNSClassifier(embedding_dim = 300,
-                      c_vocab_len = len(fcp.df["c1-c2"].unique()), 
-                      w_vocab_len = len(fcp.df["word"].unique()),
-                      lr = 1e-3,
-                      train_epocs = 20,
-                      torch_threads = 5,
-                      BCE_reduction = "mean",
-                      pred_thresh = 0.5,)
-print("training 300 dim...")
-sgns.fit(X, y)
-
-# Save model for later
-torch.save(sgns.model_.state_dict(), "sgns-300.pt")
-
-# Load model
-# model = SGNSModel(embedding_dim=300,
-#                    c_vocab_len = len(fcp.df["c1-c2"].unique()), 
-#                    w_vocab_len = len(fcp.df["word"].unique()),)
-# model.load_state_dict(torch.load("sgns-300.pt"))
