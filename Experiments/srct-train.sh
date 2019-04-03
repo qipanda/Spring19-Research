2a68f30f7ca6a70e14d067430905b855a5b167f3
qipanda <qipan321@gmail.com>
2019-04-03 14:08:47 -0400

    hyper

--- Experiments/srct-train.sh
+++ Experiments/srct-train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ENV/bin/activate
-python srct-train.py -ns 20 -K 200 -lr 1.0 -alpha 1e-2 -lam 0.0 -bs 32 -train_epochs 10 -lfpth runs
+python srct-train.py -ns 20 -K 200 -lr 1.0 -alpha 1e-2 -lam 0.0 -bs 32 -te 10 -lfpth runs
