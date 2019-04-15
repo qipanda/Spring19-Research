93cd23a7e93f08260df8d47104e175d6ffc8f443
qipanda <qipan321@gmail.com>
2019-04-15 00:33:19 -0400

    more smooth

--- Experiments/softmax-train.sh
+++ Experiments/softmax-train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ENV/bin/activate
-python softmax-train.py -g month -K 300 -lr 1.0 -alpha 5e-3 -lam 0.0 -bs 32 -te 50 -lfpth runs
+python softmax-train.py -g month -K 300 -lr 1.0 -alpha 1e-2 -lam 0.0 -bs 32 -te 50 -lfpth runs
