1254d2caeff4713aab18f71c30f92e723a4314c5
qipanda <qipan321@gmail.com>
2019-04-25 17:54:38 -0400

    hyper

--- Experiments/softmax-train.sh
+++ Experiments/softmax-train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ../ENV/bin/activate
-python softmax-train.py -g year -K 300 -lr 1.0 -alpha 1e-5 -lam 0.0 -bs 32 -te 50 -lfpth runs
+python softmax-train.py -g month -K 300 -lr 1.0 -alpha 1e-3 -lam 0.0 -bs 32 -te 50 -lfpth runs
